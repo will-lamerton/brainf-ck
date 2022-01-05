2afb79ed51da424745f9538c77d33d46f4abeee5
Will Lamerton <will@barr.media>
2022-01-05 22:36:52 +0000

    fix: wrong path to brainfuck interpreter if running without a package manager.

--- via-node.js
+++ via-node.js
@@ -1,11 +1,22 @@
-// Supporting local development and anyone not using a package manager.
-try {
-    // If using package manager.
-    const Brainfuck = require('brainf-ck/brainfuck');
-} catch(e) {
-    // If not...
-    const Brainfuck = require('brainfuck');
+/**
+ * Method to return the correct module location based on whether we might be
+ * using a package manager or not.
+ * @return {function}
+ */
+function module() {
+    try {
+        // If using package manager.
+        require.resolve('brainf-ck/brainfuck');
+        return require('brainf-ck/brainfuck');
+
+    } catch (e) {
+        // If not...
+        return require('./brainfuck');
+    }
 }
+
+// Get module.
+const Brainfuck = module();
 
 // Run...
 new Brainfuck({ viaNode: true });
